322fe5d561726db254e813eb3b2d0b2f0906532e
thiagonmiziara <thiagonmiziara@gmail.com>
2024-08-29 19:08:16 -0300

    feat: visual structure home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,26 @@
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+import { Container, Hero, Preview } from './styles'
+import previewImage from '../../assets/app-preview.png'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre.
+        </Text>
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando aplicação em funcionamento"
+        />
+      </Preview>
+    </Container>
+  )
+}
